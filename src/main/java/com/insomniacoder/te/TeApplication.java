39de42189594a9aeb76545db99503c43027b4bfd
InsomniaCoder <tanatloke@gmail.com>
2018-02-11 12:52:25 +0700

    init repo from spring initialize

---
+++ src/main/java/com/insomniacoder/te/TeApplication.java
@@ -0,0 +1,12 @@
+package com.insomniacoder.te;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeApplication.class, args);
+	}
+}
